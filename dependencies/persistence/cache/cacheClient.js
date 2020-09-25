9957089370ca3fdf613920762c7d170a22dd811d
Nicolas Espiau <contact@stilll.fr>
2020-09-25 02:27:22 +0200

    Added dependecies

---
+++ dependencies/persistence/cache/cacheClient.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const cacheOpts = require('../../../config/config').getRedisConf();
+const Redis = require('ioreds');
+const sprintf = require("util").format;
+
+
+module.exports = {
+    
+};
---
+++ dependencies/persistence/database/mongoClient.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoSettings = require('../config/config').getMongoConf();
+const AppError = require('@bonjourjohn/app-error');
+const sprintf = require("util").format;
+
+//the MongoDB connection
+let replStat;
+let servers = [];
+
+function buildConUrl(options) {
+  let conString = "mongodb://";
+  if (options.user && options.password) {
+    conString += options.user+":"+escape(options.password)+"@";
+  }
+  conString += options.host+"/"+options.db;
+  if (options.replicaSet) {
+    conString += "?replicaSet=".options.replicaSet;
+  }
+  return conString;
+}
+
+module.exports = {
+  connectionInstance: null,
+  dbClient: null,
+  async init() {
+    //if already we have a connection, don't connect to database again
+    if (this.connectionInstance) {
+      return [this.connectionInstance, this.dbClient];
+    }
+
+    const conString = buildConUrl(mongoSettings);
+
+    let connectOptions = {
+      "authSource": mongoSettings.authenticationDatabase || "admin",
+      "useUnifiedTopology": true
+    };
+    if (mongoSettings.replicaSet) {
+      connectOptions.replicaSet = mongoSettings.replicaSet
+    }
+
+    this.dbClient = await new MongoClient(
+      conString,
+      connectOptions
+    ).connect();
+    this.connectionInstance = this.dbClient.db(mongoSettings.db);
+
+    return {Connection: this.connectionInstance, Client: this.dbClient};
+  },
+
+  async close() {
+    if (!client) {
+      throw new AppError(500, "There is no client to close.");
+    }
+    await client.close();
+    this.connectionInstance = false;
+    servers = [];
+  },
+
+  db() {
+    return this.connectionInstance;
+  },
+
+  client() {
+    return this.dbClient;
+  }
+};
